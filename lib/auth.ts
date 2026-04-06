766069bc522140fab3b20d4315e0b4294c5d8adf
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-06 17:41:20 -0300

    feat: Organize authentication configuration by restructuring socialProviders

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,14 +5,19 @@ import { db } from '@/app/db'
 import * as schema from '@/app/db/schema'
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+
   emailAndPassword: {
     enabled: true,
   },
 
-  google: {
-    clientId: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
   },
+
   database: drizzleAdapter(db, {
     provider: 'pg',
     schema,
